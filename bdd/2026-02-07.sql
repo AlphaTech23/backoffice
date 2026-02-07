5e731a5967687eba2ffc40269d90d3a4955a6b9e
Noms-001 <randrichris17@gmail.com>
2026-02-07 16:19:45 +0300

    Create 2026-02-07.sql

---
+++ bdd/2026-02-07.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE "AeroAssign"
+WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'fr_FR.UTF-8'
+    LC_CTYPE = 'fr_FR.UTF-8'
+    TEMPLATE = template0;
+
+\c "AeroAssign"
