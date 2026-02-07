7033bf74acef4bbbd43cba3584fcb859c048199b
Noms-001 <randrichris17@gmail.com>
2026-02-07 17:25:25 +0300

    Update 2026-02-07.sql
    creation des tables reservation, hotel

--- bdd/2026-02-07.sql
+++ bdd/2026-02-07.sql
@@ -7,3 +7,21 @@ WITH
     TEMPLATE = template0;
 
 \c "AeroAssign"
+
+CREATE TABLE hotel (
+    id SERIAL PRIMARY KEY,
+    nom VARCHAR(150) NOT NULL
+);
+
+CREATE TABLE reservation (
+    id SERIAL PRIMARY KEY,
+    id_client VARCHAR(4) NOT NULL,
+    nombre_place INTEGER NOT NULL CHECK (nombre_place > 0),
+    date_heure_arrivee TIMESTAMP NOT NULL,
+    id_hotel INTEGER NOT NULL,
+    CONSTRAINT fk_reservation_hotel
+        FOREIGN KEY (id_hotel)
+        REFERENCES hotel(id)
+        ON UPDATE CASCADE
+        ON DELETE RESTRICT
+);
