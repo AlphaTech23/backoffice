823170290ef4d36da343289a641c96c657367858
AlphaTech23 <myahaintso@gmail.com>
2026-03-07 07:20:13 +0300

    Modification des tables

---
+++ script/update/2026-03-07.sql
@@ -0,0 +1,16 @@
+ALTER TABLE hotel ADD COLUMN code VARCHAR(10);
+
+ALTER TABLE hotel ALTER COLUMN code SET NOT NULL;
+
+ALTER TABLE type_carburant ADD COLUMN code VARCHAR(5);
+
+ALTER TABLE type_carburant ALTER COLUMN code SET NOT NULL;
+
+ALTER TABLE reservation ADD COLUMN id_trajet INTEGER;
+
+ALTER TABLE reservation ADD CONSTRAINT reservation_id_trajet_fkey
+FOREIGN KEY(id_trajet) REFERENCES trajet(id);
+
+ALTER TABLE reservation ADD COLUMN ordre INTEGER;
+
+ALTER TABLE reservation ALTER COLUMN ordre SET NOT NULL;
